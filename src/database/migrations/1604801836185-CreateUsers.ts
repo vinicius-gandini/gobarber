e9ee4e53d09700e25db403b95283d726827c9fac
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-11-08 22:07:14 -0300

    feat: relationship between user and appointment :ok_hand:

--- src/database/migrations/1604801836185-CreateUsers.ts
+++ src/database/migrations/1604801836185-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1604801836185 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
