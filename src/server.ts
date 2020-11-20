d6c013b0bf7656b69d7af360550b1fea5ce042a3
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-11-19 23:11:38 -0300

    feat: :ok_hand: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -9,6 +10,7 @@ import AppError from './errors/AppError';
 import './database';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
