5f9c93c3988216969e2d89dcd3978698a296ad4b
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-12-05 21:24:37 -0300

    test: :bomb: recover password email error non existing user

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -43,7 +43,7 @@ describe('AuthenticateUser', () => {
       fakeHashProvider,
     );
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: '123456',
