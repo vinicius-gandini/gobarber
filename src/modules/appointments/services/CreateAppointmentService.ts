28bb3c1fb78a7b332d252457d5abf3af47250833
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-12-24 20:53:31 -0300

    fix: :wrench: changed findbydate method to consider provider id

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -40,6 +40,7 @@ class CreateAppointmentService {
 
     const findAppoinmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
+      provider_id,
     );
 
     if (findAppoinmentInSameDate) {
